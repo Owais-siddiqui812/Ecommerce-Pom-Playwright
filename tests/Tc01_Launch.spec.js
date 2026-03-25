6ea4f9c6a34a4492cf9fd75a4e1aa3d000f5b23b
owaissid <owaissid001@gmail.com>
2026-03-25 15:10:57 +0530

    Changes in code

--- tests/Tc01_Launch.spec.js
+++ tests/Tc01_Launch.spec.js
@@ -1,7 +1,7 @@
 const {test,expect}=require("@playwright/test");
 const {Homepage}=require("../pages/Homepage");
 
-test("@sanity @ regression TC01_Launch",async({page})=>{
+test("@sanity @regression TC01_Launch",async({page})=>{
 const home=new Homepage(page);
 await home.gotopage();
 await home.pageverify(expect,"Your store of fun");
